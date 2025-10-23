86858136ca315f69fe9447b668db0035ce61d38f
JuanMartinezB <juans.martinezb@uqvirtual.edu.co>
2025-10-23 06:09:01 -0500

    Interfaz de Inició de Sesión (Falta por terminar)

---
+++ app/src/app/pages/home-public/home-public.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-public',
+  imports: [],
+  templateUrl: './home-public.html',
+  styleUrl: './home-public.css',
+})
+export class HomePublic {
+
+}
